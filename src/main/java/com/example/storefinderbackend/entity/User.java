63a93b487164b9fe8b50dea8969c021e6ff6f706
Archana-ab <archanabamel09@gmail.com>
2024-05-17 14:40:51 +0530

    Jwt

--- src/main/java/com/example/storefinderbackend/entity/User.java
+++ src/main/java/com/example/storefinderbackend/entity/User.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,8 +24,82 @@ public class User {
     private String username;
     @Column(name = "email_id", nullable = false, unique = true)
     private String email;
+
+    private String password;
     @Column(name = "role")
     private String role;
     @Column(name = "reg_date")
     private String registrationDate;
+
+    private LocalDateTime createdAt;
+
+//    public User(){
+//
+//    }
+//    public User(Long id, String username, String email, String password, String role, String registrationDate, LocalDateTime createdAt){
+//        super();
+//        this.id=id;
+//        this.username=username;
+//        this.email=email;
+//        this.password=password;
+//        this.role=role;
+//        this.registrationDate=registrationDate;
+//        this.createdAt=createdAt;
+//    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
---
+++ src/main/java/com/example/storefinderbackend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.storefinderbackend.repository;
+
+import com.example.storefinderbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    public User findByEmail(String email);
+}
