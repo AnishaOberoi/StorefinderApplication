87b2f8006485c7f84bf64e8214270abac8bb6554
Archana-ab <archanabamel09@gmail.com>
2024-05-19 23:34:40 +0530

    Category Entity

---
+++ src/main/java/com/example/storefinderbackend/entity/Category.java
@@ -0,0 +1,52 @@
+package com.example.storefinderbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ElementCollection
+    private List<Long> storeIds = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Long> getStoreIds() {
+        return storeIds;
+    }
+
+    public void setStoreIds(List<Long> storeIds) {
+        this.storeIds = storeIds;
+    }
+}
